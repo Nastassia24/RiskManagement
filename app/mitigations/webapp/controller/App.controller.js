1be0dddf8f4a9231619c87df3175643e16078209
Nastassia Ziamliakova <nastassia.ziamliakova@sap.com>
2023-12-19 19:21:51 +0100

    Init commit

---
+++ app/mitigations/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("mitigations.mitigations.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
